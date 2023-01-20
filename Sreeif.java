2c07508c8f933eeebcad7790edac2e3f77487327
sriswe <triplejems007@gmail.com>
2023-01-20 18:04:06 +0530

    sreeif new file is added

---
+++ Sreeif.java
@@ -0,0 +1,24 @@
+package Sree;
+
+public class Sreeif {
+	public static void main (String []args) {
+	int num1 =3;
+	int num2 =2;
+	int num3 =10;
+	
+	
+	//If block
+	if((num1>num2) && (num1>num3)) {
+		System.out.println(num1+" =  is greatest of all");
+	}else if((num2>num1) && (num2>num3)) {
+		System.out.println(num2+" =  is greatest of all");
+	}else {
+		System.out.println(num3+" =  is greatest of all");
+	}
+	
+	
+	
+}
+
+}
+
